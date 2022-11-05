0dba705c2f768fa37f1e67271460ff60ba47b678
FIKRIIXYZ <116017493+FIKRIIXYZ@users.noreply.github.com>
2022-11-05 20:19:22 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,47 +1,57 @@
+let moment = require('moment-timezone')
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
+let fs = require('fs')
 let fetch = require('node-fetch')
+let handler = async (m, {text}) => {
+if (!text) return conn.reply(m.chat, 'Harap masukkan link\n\nContoh: .tiktok https://tiktok.com/xxxxxx', m)
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-bear = "9b95802c6f20";
-  if (!text) return conn.reply(m.chat, `Gunakan format: ${usedPrefix}${command} Colin Changed`, m)
+    let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+    else who = m.sender
+    let user = global.db.data.users[who]
+    let more = String.fromCharCode(8206)
+    let readMore = more.repeat(4001)
+let anu = `*── 「 TIKTOK 」 ──*
 
-  await conn.sendButtonLoc(m.chat, 'https://telegra.ph/file/5a46383499a405792185b.jpg', 'Searching...', wm2, 'Owner', '.owner', m)
-    let res = await fetch(`https://zenzapis.xyz/downloader/musically?apikey=${bear}&url=${text}`)
-    let json = await res.json()
-    if (res.status !== 200) throw await res.text()
-    if (!json.status) throw json
-const ftrol = {
-    key : {
-    remoteJid: 'status@broadcast',
-    participant : '0@s.whatsapp.net'
-    },
-    message: {
-    orderMessage: {
-    itemCount : 2022,
-    status: 1,
-    surface : 1,
-    message: `Search Tiktok NoWM 🔍`, 
-    orderTitle: ``,
-    thumbnail: await (await fetch('https://telegra.ph/file/f13cee700cf48fdda942d.jpg')).buffer(), //Gambarnye
-    sellerJid: '0@s.whatsapp.net' 
-    }
-    }
-    }
-    conn.sendButtonVid(m.chat, json.result.nowm, `Nih Kak @${m.sender.split`@`[0]}`, wm, 'Audio', `!tiktokmp3 ${text}`, m, {
-    quoted: ftrol, contextInfo: { forwardingScore: 99999, isForwarded: true,
-         externalAdReply: { 
-             title: global.wm,
-             body: 'Apa Benar Ini Yang Ada Cari?',
-             description: 'Apa Benar Ini Yang Ada Cari?', 
-             mediaType: 2, 
-           thumbnail: await (await fetch('https://i.ibb.co/jfZVKmC/babi2.jpg')).buffer(), 
-          mediaUrl: `https://youtu.be/PPLlC5NWAuE` 
-         } 
-      } 
-   })
+SILAHKAN PILIH`
+     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+     templateMessage: {
+         hydratedTemplate: {
+           hydratedContentText: anu,
+           locationMessage: { 
+           jpegThumbnail: fs.readFileSync('./src/welcome.jpg')}, 
+           hydratedFooterText: wm,
+           hydratedButtons: [{
+             urlButton: {
+               displayText: '📍instagram',
+               url: instagram
+               }
+               
+             },
+             {
+             quickReplyButton: {
+               displayText: 'Tanpa Watermark',
+               id: `.tiktoknowm ${text}`,
+             }
+           },
+           {
+             quickReplyButton: {
+               displayText: 'dengan watermark',
+               id: `.tiktokwm ${text}`,
+             }
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
 }
-handler.help = ['tiktok <keyword>'] 
- handler.limit = true 
- handler.tags = ['downloader'] 
- handler.command = /^(tiktok)$/i 
-  
- module.exports = handler
+handler.help = ['tiktok'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^(tiktok)$/i
+
+module.exports = handler
