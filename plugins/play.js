6e45dbbe8ed6ddc43e9bd49a2040d0a315a6c834
FIKRIIXYZ <116017493+FIKRIIXYZ@users.noreply.github.com>
2022-11-08 17:38:47 +0700

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -1,91 +1,30 @@
-const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
-const { servers, yta, ytv } = require('../lib/y2mate')
-let fs = require('fs')
+let limit = 30
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
 let yts = require('yt-search')
 let fetch = require('node-fetch')
-let handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} california`
-  let chat = global.db.data.chats[m.chat]
+const { servers, yta, ytv } = require('../lib/y2mate')
+let handler = async (m, { itsu, command, text, isPrems, isOwner }) => {
+  if (!text) throw 'Cari apa?'
   let results = await yts(text)
   let vid = results.all.find(video => video.seconds < 3600)
-  if (!vid) throw 'Konten Tidak ditemukan'
+  if (!vid) throw 'Video/Audio Tidak ditemukan'
   let isVideo = /2$/.test(command)
-  let yt = false
-  let yt2 = false
-  let usedServer = servers[0]
-  for (let i in servers) {
-    let server = servers[i]
-    try {
-      yt = await yta(vid.url, server)
-      yt2 = await ytv(vid.url, server)
-      usedServer = server
-      break
-    } catch (e) {
-      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\nmencoba server lain...'}`)
-    }
-  }
-  if (yt === false) throw 'semua server gagal'
-  if (yt2 === false) throw 'semua server gagal'
-  let { dl_link, thumb, title, filesize, filesizeF } = yt
-let anu =  `
-*Judul:* ${title}
-*Ukuran File Audio:* ${filesizeF}
-*Ukuran File Video:* ${yt2.filesizeF}
-*Server y2mate:* ${usedServer}
-*link sumber:* 
-${vid.url}
-
-`
-     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
-     templateMessage: {
-         hydratedTemplate: {
-           hydratedContentText: anu,
-           locationMessage: { 
-           jpegThumbnail: await (await fetch(thumb)).buffer() }, 
-           hydratedFooterText: `jika video tidak sesuai
-silahkan ketik .yta link YouTube untuk mengunduh audio
-dan ketik .ytv link YouTube Untuk Mengunduh Video`,
-           hydratedButtons: [{
-             urlButton: {
-               displayText: '📍instagram',
-               url: instagram
-             }
-
-           },
-               {
-             quickReplyButton: {
-               displayText: 'video 360p',
-               id: `.ytmp4 ${vid.url}`,
-             }
-
-            },
-               {
-             quickReplyButton: {
-               displayText: 'video 720p',
-               id: `.ytv720 ${vid.url}`,
-             }
-
-            },
-               {
-             quickReplyButton: {
-               displayText: 'Audio',
-               id: `.ytmp3 ${vid.url}`,
-             }
-
-           }]
-         }
-       }
-     }), { userJid: m.sender, quoted: m });
-    //conn.reply(m.chat, text.trim(), m)
-    return await conn.relayMessage(
-         m.chat,
-         template.message,
-         { messageId: template.key.id }
-     )
+  let { dl_link, thumb, title, filesize, filesizeF} = await (isVideo ? ytv : yta)(vid.url, 'id4')
+  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
+  itsu.sendMessage(m.chat, `*Title:* ${title}\n*Size:* ${filesizeF}` , 'conversation', {quoted: m, thumbnail: global.thumb2, contextInfo:{externalAdReply: {title: 'Simple WhatsApp bot', body: `© ${itsu.user.name}`, sourceUrl: '', thumbnail: global.thumb}}})
+  let _thumb = {}
+  try { if (isVideo) _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
+  catch (e) { }
+  if (!isLimit) itsu.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(command)), `
+*Title:* ${title}
+*Filesize:* ${filesizeF}
+*Source:* ${vid.url}
+`.trim(), m, false, {ptt: true, duration: 400000}, _thumb || {})
 }
-handler.help = ['play'].map(v => v + ' <pencarian>')
+handler.help = ['play', 'play2'].map(v => v + ' <pencarian>')
 handler.tags = ['downloader']
-handler.command = /^(p|play)$/i
+handler.command = /^play2?$/i
 
 handler.exp = 0
 
