041d2daf7a18b54e636c848118b6747d9eb5c5ca
FIKRIIXYZ <116017493+FIKRIIXYZ@users.noreply.github.com>
2022-11-08 18:48:48 +0700

    Update shorturl.js

--- plugins/shorturl.js
+++ plugins/shorturl.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
   if (!text) throw 'url/link nya mana?'
-  let res = await fetch(`http://hadi-api.herokuapp.com/api/shorturl?url=${text}`)
+  let res = await fetch(`https://api.lolhuman.xyz/api/shortlink2?apikey=f52719f8e573fef99020268f&url=${text}`)
   let json = await res.json()
   if (json.status) m.reply(json.result)
   else throw 'Link Invalid!\nPeriksa url anda'
